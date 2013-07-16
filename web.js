a4f78af67eb8358570e9445908b79bd6cf6f34c1
kevindeland <kevin.deland@gmail.com>
2013-07-16 22:11:47 +0000

    Configured web.js to read index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('index.html');
+    var message = fs.readFileSync('index.html').toString();
+    response.send(message);
 });
 
 var port = process.env.PORT || 5000;
